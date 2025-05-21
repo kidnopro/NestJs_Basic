51f0439cfab42c6ffd134cd5dfa6ae27f8df63f9
kidnopro <hoanganhfullstack@gmail.com>
2025-05-21 10:56:09 +0700

    Chore: connect db postgre

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'cucucu99',
+      database: 'dayladb',
+      entities: [],
+      synchronize: true, // Tự động tạo table, chỉ dùng trong môi trường đép
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
